3112252d992421677881adb5e411808808e95d07
zelu96 <47740805+zelu96@users.noreply.github.com>
2020-03-10 17:08:05 +0100

    Add files via upload

---
+++ zajecia wtorek 10.03/zajecia/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int silnia(int n)
+{
+	if(n>0)
+	{
+		int wynik=1;
+		for(int i = 1; i<=n; i++)
+		{
+			wynik*=i;
+		}
+		return wynik;
+	}
+}
+
+
+int main()
+{
+    int liczba;
+    printf("Podaj liczbe: ");
+    scanf("%d",&liczba);
+    printf("%d",silnia(liczba));
+
+    return 0;
+}
